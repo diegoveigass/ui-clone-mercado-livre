4bf0f179b48beca5e3157e861ee2bbe0b60a4b4a
Diego Veiga Santos <sdiegoveiga@gmail.com>
2020-09-03 11:22:00 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import GlobalStyles from "./styles/GlobalStyles";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <h1>Ola</h1>
+    </>
+  );
+}
+
+export default App;
